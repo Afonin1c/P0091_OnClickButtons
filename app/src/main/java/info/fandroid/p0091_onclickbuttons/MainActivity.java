e1882b632f072ce7312480581254887f6c3753d3
androidstrart <nepochatoff@gmail.com>
2015-05-06 17:17:57 +0300

    Initial commit

---
+++ app/src/main/java/info/fandroid/p0091_onclickbuttons/MainActivity.java
@@ -0,0 +1,75 @@
+package info.fandroid.p0091_onclickbuttons;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    TextView textView;
+    Button button1;
+    Button button2;
+    Button button3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Нажата кнопка 1");
+            }
+        });
+
+        button2.setOnClickListener(this);
+
+
+
+
+    }
+
+    public void clickButton3(View view) {
+        textView.setText("Нажата кнопка 3");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        textView.setText("Нажата кнопка 2");
+    }
+}
